35c8a7759a1b5cbbe516c6398d507b4b68931026
Shengfa <lshengfa@umich.edu>
2015-03-24 17:31:30 -0400

    added some basic files to fill, has url retrieving

--- src/collect_repos.py
+++ src/collect_repos.py
@@ -1,14 +1,51 @@
 import requests
 import json
+import time
+from datetime import date
+import download_repo
+#today = date.today()
+#print today
+def collect_repo_urls():
+	language = 'C'
+	size_limit = '1000'
+	starting_date = date(2009,1,1)
+	per_page = 100
+	page_num = 1
+	compress_format = 'tarball'
 
-#&sort=created&order=asc
-query_url = 'https://api.github.com/search/repositories?q=language:C+size:<=1000&per_page=100&page=10';
-r = requests.get(query_url)
-print r.headers
-if(r.ok):
-	repoItem = json.loads(r.text or r.content)
-	print len(repoItem)
-	#print repoItem
-	print len(repoItem['items'])
-	for item in repoItem['items']:
-		print item
+	url_list = []
+	print str(starting_date)
+
+
+
+	#&sort=created&order=asc
+	#query_url = 'https://api.github.com/search/repositories?q=language:C+size:<=1000&per_page=100&page=10';
+	query_url = ("https://api.github.com/search/repositories?"
+		     "q=language:"+language +
+		     "+size:<=" + size_limit +
+		     "+created:<"+str(starting_date) +
+		     "&per_page=" + str(per_page) +
+		     "&page=" + str(page_num)
+		    )
+	print query_url
+	r = requests.get(query_url)
+	print r.headers
+	if(r.ok):
+		repoItem = json.loads(r.text or r.content)
+		repoList = repoItem['items']
+		print len(repoItem)
+		#print repoItem
+		print len(repoItem['items'])
+		for item in repoList:
+	#		print item['url']
+			url = item['url'] + '/' + compress_format
+	#		print url
+			url_list.append(url)
+	#		print item
+	return url_list
+	#print repoList[0]
+
+if __name__ == '__main__':
+	url_list = collect_repo_urls()
+	for url in url_list:
+		download_repo.download_url(url)
---
+++ src/download_repo.py
@@ -0,0 +1,6 @@
+def download_url(url): 
+	print url
+
+if __name__ == '__main__':
+	download_url(url)
+	
