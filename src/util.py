f4b564e4b1dd760a61e9b296ffcac150148fc095
Nina <ninazeng@umich.edu>
2015-03-28 19:03:39 -0400

    removing tar util function

--- src/util.py
+++ src/util.py
@@ -1,4 +1,6 @@
+import os
 from subprocess import call
+from glob import glob
 def make_folder(folder_name):
 	print 'Creating folder ' + folder_name + '...'
 	call(["mkdir", folder_name])
@@ -10,3 +12,18 @@ def make_file(file_name):
 	echo_line = 'echo "{}" >> ' + file_name
 	print echo_line
 	call(echo_line, shell=True)  
+
+def delete_files_with_extension(ext, path=None):
+	print "Deleteing files with extension " + ext
+	victim = ""
+	if path is not None:
+		victim = path + "/*." + ext
+	else:
+		victim = "*." + ext
+	for v in glob(victim):
+		os.remove(v)
+# deletes all tars in the path directory
+# if no path is provided delete ones in current dir
+def delete_tarballs(dirpath=None):	
+	delete_files_with_extension("tar.gz", dirpath)
+
