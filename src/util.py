917ca3779a0d0bf86bf29f71840c9ae2b4743551
Nina <ninazeng@umich.edu>
2015-04-07 18:44:33 -0400

    fixed a bug in util

--- src/util.py
+++ src/util.py
@@ -49,15 +49,15 @@ def find_extensions(ext, path = None):
 
 
 
-# deletse all files/subfolder contents in a directory
-def delete_in_directory(folder):
+def delete(folder):
 	# folder = '/path/to/folder'
 	for the_file in os.listdir(folder):
-	    file_path = os.path.join(folder, the_file)
-        try:
-		    if os.path.isfile(file_path):
+		file_path = os.path.join(folder, the_file)
+		try:
+			if os.path.isfile(file_path):
 				os.unlink(file_path)
-            elif os.path.isdir(file_path): shutil.rmtree(file_path)
-	    except Exception, e:
-		    print e
+			elif os.path.isdir(file_path): 
+				shutil.rmtree(file_path)
+		except Exception, e:
+			print e
 
